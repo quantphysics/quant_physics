aedbd5a5f2c70f17cdcc17117f5c7f1f4281cf15
clarkmiyamoto <cmiyamot@usc.edu>
2022-09-22 22:30:17 -0700

    added pathing

---
+++ quant_physics/PortfolioConstruction/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Quant Physics.
+#
+# (C) Copyright Quant Physics 2022.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
---
+++ quant_physics/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Quant Physics.
+#
+# (C) Copyright Quant Physics 2022.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Quant Physics"""
+__version__ = '0.0.1'
+__copyright__ = 'Copyright Quant Physics 2022'
+__author__ = 'Clark Miyamoto, ...'
+__status__ = "Development"
+
+
+###########################################################################
+### User-accessible scope
+
+from . import Execution
+from . import PortfolioConstruction
+from . import TransactionModel
+from . import RiskModel
+from . import AlphaModel

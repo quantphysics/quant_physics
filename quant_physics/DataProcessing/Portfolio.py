89837bbded7ce4ed08e855e8563d915da41aea5a
Tetsuto Nagashima <tetsnaga@usc.edu>
2023-01-01 19:14:24 -0800

    Update Portfolio.py
    Added definitions for importJSON, exportJSON, printTransactions.
    Created init and dict functions for Transactions class
    Created visualization method for earnings vs time for a single stock

--- quant_physics/DataProcessing/Portfolio.py
+++ quant_physics/DataProcessing/Portfolio.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import json
+import matplotlib.pyplot as plt
 
 class Portfolio:
     '''
@@ -10,15 +11,38 @@ class Portfolio:
     '''
     Methods
     '''
+    
+    # input: filename
+    # output: writes to file
     def exportJSON(filename):
-        # input: name of file
-        # output: write to desired file
+        # store the transactions and assets in a JSON object
+        json_data = json.dumps({
+            'transactionArray': self.transactionArray,
+            'assetsSet': list(self.assetsSet)
+        })
+
+        # write JSON object to file
+        with open(filename, 'w') as outfile:
+            outfile.write(json_data)
+            
         return 0
-        
+    
+    # input: filename
+    # output: portfolio object
     def importJSON(filename):
-        # input: filename or path
-        # output: ???
-        return 0
+        # read JSON data from the file
+        with open(filename, 'r') as infile:
+            json_data = infile.read()
+        
+        # deserialize JSON data
+        data = json.loads(json_data)
+        
+        # create new Portfolio object with the deserialized data
+        portfolio = Portfolio()
+        portfolio.transactionArray = data['transactionArray']
+        portfolio.assetsSet = set(data['assetsSet'])
+        
+        return portfolio
     
     def buy(name, quantity):    
         # update assetsSet
@@ -34,17 +58,45 @@ class Portfolio:
         return 0
         
     def printTransactions():
-        # loops through transactionsArray and prints in desired format
+        # loop through the transactions and prints in desired format
+        for transaction in self.transactionArray:
+            print(f'Type: {transaction.type}, Quantity: {transaction.quantity}, Time: {transaction.time}, Price: {transaction.price}')
         return 0
     
     def totalValue():
         # loop through stocks owned, stockAmount * stockPrice
         return 0
     
-    # Graphing methods and data visualization
-    def printEarningsVsTime():
+# Graphing methods and data visualization
+    
+    # input: stock ticker, start and end date, and quantity
+    # output: earnings vs time graph for that stock
+    def stockEarningsVsTime(self, ticker, start_date, end_date, quantity):
         # input: ticker, start date, end date, quantity graphed
         # print changes in earnings over time, etc.
+        
+        # get stock data
+        stock_data = yf.Ticker(ticker).history(start=start_date, end=end_date)
+        
+        # calculate earnings at each point in time
+        earnings = []
+        for index, row in stock_data.iterrows():
+            # Calculate the earning for the current time point
+            earning = row['Close'] * quantity - row['Open'] * quantity
+            earnings.append(earning)
+        
+        # Create a plot of the earnings over time
+        plt.plot(stock_data.index, earnings)
+        plt.xlabel('Time')
+        plt.ylabel('Earnings')
+        plt.show()
+        
+        return 0
+    
+    # input: start and end date
+    # output: graph of entire portfolio earnings vs time
+    def portfolioEarningsVsTime(self, start_date, end_date):
+        # TODO: Define totalValue() as a function of start and end date, default start and end date is entire period of buying/selling
         return 0
 
     # nested class for transactions
@@ -58,5 +110,21 @@ class Portfolio:
         quantity = 0
         time = 0  # Time you bought asset: Pandas Series .index should give date
         price = 0
+        
+        # create constructor so we can read in as JSON
+        def __init__(self, data):
+            self.type = data['type']
+            self.quantity = data['quantity']
+            self.time = data['time']
+            self.price = data['price']
+            
+        # create dictionary method so we can export as JSON
+        def __dict__(self):
+            return {
+                'type': self.type,
+                'quantity': self.quantity,
+                'time': self.time,
+                'price': self.price
+            }
         
         

4b172d7ebd9ec45da1ac8285451f1fbe702b8740
Tetsuto Nagashima <tetsnaga@usc.edu>
2023-01-02 15:20:05 -0800

    Remove Transactions class
    Moved to separate file

--- quant_physics/DataProcessing/Portfolio.py
+++ quant_physics/DataProcessing/Portfolio.py
@@ -98,33 +98,3 @@ class Portfolio:
     def portfolioEarningsVsTime(self, start_date, end_date):
         # TODO: Define totalValue() as a function of start and end date, default start and end date is entire period of buying/selling
         return 0
-
-    # nested class for transactions
-    class Transaction:
-        type = 0
-        '''
-        “purchase” = +1
-        “sell” = -1
-        Note: this will make calculations much easier, e.g. to add up total amount of a stock, just add up all values of (type * quantity) without need for if statements
-        '''
-        quantity = 0
-        time = 0  # Time you bought asset: Pandas Series .index should give date
-        price = 0
-        
-        # create constructor so we can read in as JSON
-        def __init__(self, data):
-            self.type = data['type']
-            self.quantity = data['quantity']
-            self.time = data['time']
-            self.price = data['price']
-            
-        # create dictionary method so we can export as JSON
-        def __dict__(self):
-            return {
-                'type': self.type,
-                'quantity': self.quantity,
-                'time': self.time,
-                'price': self.price
-            }
-        
-        

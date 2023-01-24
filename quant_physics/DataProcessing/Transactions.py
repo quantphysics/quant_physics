7d99b43d6e8c766c45f86c073f368501b1ce501f
Tetsuto Nagashima <tetsnaga@usc.edu>
2023-01-24 15:37:00 -0800

    Remove unnecessary code

--- quant_physics/DataProcessing/Transactions.py
+++ quant_physics/DataProcessing/Transactions.py
@@ -1,17 +1,13 @@
 import json
 
 class Transaction:
-    type = ""  # "buy" or "sell"
-    quantity = 0
-    time = ""  # Time you bought asset: Pandas Series .index should give date
-    price = 0
-
     # create constructor so we can read in as JSON
     def __init__(self, data):
         self.type = data['type']
         self.quantity = data['quantity']
         self.time = data['time']
         self.price = data['price']
+        self.ticker = data['ticker']
 
     # create dictionary method so we can export as JSON
     def __dict__(self):
@@ -19,5 +15,6 @@ class Transaction:
             'type': self.type,
             'quantity': self.quantity,
             'time': self.time,
-            'price': self.price
+            'price': self.price,
+            'ticker': self.ticker
         }
